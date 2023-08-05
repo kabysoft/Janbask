abf7f54d7edd5ed9c8ee71d27a039b5167b770e4
kabysoft <kabysoft@yahoo.com>
2023-08-05 01:08:50 -0600

    This is my second commit

---
+++ ClassOne/src/StudentDetails.java
@@ -0,0 +1,18 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String Stdid = "001";
+        String StdfirstName ="Sigrid";
+        String StdlastName = "Gate";
+        char StdGender = 'F';
+        int StdAge = 17;
+        int StdBusFee = 10;
+
+        System.out.println("Student Id :"+Stdid);
+        System.out.println("Student First Name :"+ StdfirstName);
+        System.out.println("Student Last Name :"+StdlastName);
+        System.out.println("Student Gender :"+ StdGender);
+        System.out.println("Student Age :"+ StdAge + " Years Old");
+        System.out.println("Student Bus Fee :"+ StdBusFee +" $");
+
+    }
+}
