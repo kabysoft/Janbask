d62a595fea541a75e43339f7084486cd38059820
kabysoft <kabysoft@yahoo.com>
2023-08-13 00:02:16 -0600

    Switch CAse Bloc

---
+++ ClassOne/src/SwitchCase/SwithcCAseScan.java
@@ -0,0 +1,57 @@
+package SwitchCase;
+
+import java.util.Scanner;
+
+public class SwithcCAseScan {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        switch (month) {
+            case(1):
+                System.out.println("The Actual month is : January");
+                break;
+            case(2):
+                System.out.println("The Actual month is : February");
+                break;
+            case(3):
+                System.out.println("The Actual month is : March");
+                break;
+            case(4):
+                System.out.println("The Actual month is : April");
+                break;
+            case(5):
+                System.out.println("The Actual month is : May");
+                break;
+            case(6):
+                System.out.println("The Actual month is : Jun");
+                break;
+            case(7):
+                System.out.println("The Actual month is : July");
+                break;
+            case(8):
+                System.out.println("The Actual month is : August");
+                break;
+            case(9):
+                System.out.println("The Actual month is : September");
+                break;
+            case(10):
+                System.out.println("The Actual month is : October");
+                break;
+            case(11):
+                System.out.println("The Actual month is : November");
+                break;
+            case(12):
+                System.out.println("The Actual month is : December");
+                break;
+
+            default:
+                System.out.println("Please insert a correct value between 1 to 12 ");
+                break;
+
+        }
+        System.out.println("Out of the bloc switch case ");
+
+
+    }
+
+}
